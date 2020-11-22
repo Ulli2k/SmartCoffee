c351e1e5aee005eb135ef128d278991bb31fb60c
Ulli <14141949+Ulli2k@users.noreply.github.com>
2020-11-22 17:57:07 +0100

    Update web_global.h

--- include/web_global.h
+++ include/web_global.h
@@ -9,9 +9,11 @@ enum INTERFACETYPE { MQTT, WEBPAGE };
 const char ssid[]     = "";
 const char password[] = "";
 
-#define FHEM_SERVER_URL                "http://xxx"
-#define FHEM_POST_MESSAGE_ON           "attr notify_SmartCoffee disable 1;set SmartCoffee on;attr notify_SmartCoffee disable 0"
-#define FHEM_POST_MESSAGE_OFF          "attr notify_SmartCoffee disable 1;set SmartCoffee off;attr notify_SmartCoffee disable 0"
+#define FHEM_SERVER_URL                "http://192.168.188.42:8083/fhem"
+//#define FHEM_POST_MESSAGE_ON           "attr notify_SmartCoffee disable 1;set SmartCoffee on;attr notify_SmartCoffee disable 0"
+#define FHEM_POST_MESSAGE_ON           "setstate SmartCoffee on"
+//#define FHEM_POST_MESSAGE_OFF          "attr notify_SmartCoffee disable 1;set SmartCoffee off;attr notify_SmartCoffee disable 0"
+#define FHEM_POST_MESSAGE_OFF          "setstate SmartCoffee off"
 
 #define MQTT_BROCKER_IP                "xx"
 #define MQTT_PORT                       1883
